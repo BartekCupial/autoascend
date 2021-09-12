2e93111dc376e68ef27a2296be32e8617fa9d095
Maciej Sypetkowski <maciej.sypetkowski@gmail.com>
2021-09-12 19:10:22 +0000

    Allow full valkyrie & knight runs

--- heur/run.py
+++ heur/run.py
@@ -37,7 +37,8 @@ class EnvWrapper:
         obs, reward, done, info = self.env.step(nh.actions.ACTIONS.index(action))
         self.score += reward
         self.step_count += 1
-        if self.score > 3000:
+        if self.score > 3000 and \
+                self.agent.character.role not in [self.agent.character.VALKYRIE, self.agent.character.KNIGHT]:
             for _ in range(5):
                 action = nh.actions.ACTIONS.index(nh.actions.Command.ESC)
                 obs, reward, done, info = self.env.step(action)
