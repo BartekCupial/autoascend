e8911f76450d120aa891eea30f28a1c4977335fd
Maciej Sypetkowski <maciej.sypetkowski@gmail.com>
2021-10-13 20:10:10 +0000

    Remove score limit

--- heur/run.py
+++ heur/run.py
@@ -37,15 +37,15 @@ class EnvWrapper:
         obs, reward, done, info = self.env.step(nh.actions.ACTIONS.index(action))
         self.score += reward
         self.step_count += 1
-        if self.score > 4500:
-            if self.agent.character.role not in []:#self.agent.character.VALKYRIE]:
-                for _ in range(5):
-                    action = nh.actions.ACTIONS.index(nh.actions.Command.ESC)
-                    obs, reward, done, info = self.env.step(action)
-                for c in '#quit\ry':
-                    action = nh.actions.ACTIONS.index(ord(c))
-                    obs, reward, done, info = self.env.step(action)
-                assert done
+        #if self.score > 4500:
+        #    if self.agent.character.role not in []:#self.agent.character.VALKYRIE]:
+        #        for _ in range(5):
+        #            action = nh.actions.ACTIONS.index(nh.actions.Command.ESC)
+        #            obs, reward, done, info = self.env.step(action)
+        #        for c in '#quit\ry':
+        #            action = nh.actions.ACTIONS.index(ord(c))
+        #            obs, reward, done, info = self.env.step(action)
+        #        assert done
         return obs, reward, done, info
 
     def debug_tiles(self, *args, **kwargs):
