a0d6486c1228d87e25023c4ce0c661d8142f0651
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-04-15 15:21:03 +0200

    add formatting

---
+++ heur/run_formatting.py
@@ -0,0 +1,144 @@
+import jsonlines
+import os
+import sys
+import traceback
+
+from multiprocessing import Pool
+from pathlib import Path
+from argparse import ArgumentParser
+
+from heur.action_textmap import (
+    nle_comp_preqs,
+    nle_obs_preqs,
+    special_tokens_interaction_history,
+)
+from heur.instruction_encode_templates import *
+
+
+def form_prompt(data, obs_preqs):
+    return "\n".join(
+        [
+            "%s[\n%s\n]" % (obs_preqs[key], data[key])
+            for key in (
+                "txt_blstats",
+                "txt_glyphs",
+                "txt_message",
+                "txt_inventory",
+                "txt_cursor",
+            )
+        ]
+    )
+
+
+def faster_load_and_process_chunks(
+    input_dir,
+    output_dir,
+    observation_tok=special_tokens_interaction_history["observation"],
+    obs_preqs=nle_obs_preqs,
+    comp_preqs=nle_comp_preqs,
+    instruction="You are an agent playing NetHack. Predict the next actions.",
+):
+    def process_helper_raw_observation(data):
+        seq = len(data)
+            
+        query = form_prompt(data[0], obs_preqs)
+        prompts = []
+        actions = []
+        for i in range(seq):
+            if i < seq - 1:
+                prompts += [form_prompt(data[i + 1], obs_preqs=obs_preqs)]
+            actions += [data[i]["txt_action"]]
+
+        obs = []
+        for i in range(seq - 1):
+            obs += [prompts[i]]
+
+        completion = ""
+        for j in range(seq):
+            completion += "\n%s%s" % (
+                comp_preqs["action"],
+                actions[j],
+            )
+            if j < seq - 1:
+                completion += "\n%s\n%s" % (observation_tok, obs[j])
+
+        return encode_instruction_example(
+            instruction,
+            query,
+            completion,
+            random_template=False,
+            eos_token=None,
+        )
+
+    input_dir = Path(input_dir)
+    output_dir = Path(output_dir)
+    output_file = output_dir / (input_dir.name + ".jsonl")
+    output_file.parent.mkdir(exist_ok=True, parents=True)
+
+    for file in input_dir.iterdir():
+        with jsonlines.open(file, "r") as reader:
+            chunks = []
+            for i, dataum in enumerate(reader):
+                chunks += [dataum]
+
+            raw_histories = process_helper_raw_observation(chunks[1:])
+
+            with jsonlines.open(output_file, "a") as writer:
+                writer.write_all([raw_histories])
+
+    return 1
+
+
+def worker(args):
+    from_, to_, input_dirs, output_dir = args
+    
+    scores = []
+    for i in range(from_, to_):
+        input_dir = input_dirs[i]
+        
+        try:
+            faster_load_and_process_chunks(
+                input_dir=input_dir,
+                output_dir=output_dir,
+            )
+        except BaseException as e:
+            print(''.join(traceback.format_exception(None, e, e.__traceback__)), file=sys.stderr)
+
+    return scores
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--input_dir",
+        type=str,
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+    )
+    parser.add_argument("--num_workers", default=16, type=int)
+
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    os.makedirs(args.output_dir, exist_ok=True)
+    directories = list(Path(args.input_dir).iterdir())
+
+    with Pool(args.num_workers) as pool:
+        scores = list(
+            pool.map(worker, [
+                (
+                    i * len(directories) // args.num_workers,
+                    (i + 1) * len(directories) // args.num_workers,
+                    directories,
+                    args.output_dir,
+                )
+                for i in range(args.num_workers)
+            ]
+        ))
---
+++ run_formatting.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 heur/run_formatting.py --input_dir=text_data --output_dir=processed_data --num_workers=8
