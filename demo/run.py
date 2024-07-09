270af02e46137280360adda2814dc6bb9b3d8643
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-07-09 17:58:19 +0200

    print flags

--- demo/run.py
+++ demo/run.py
@@ -70,6 +70,7 @@ if __name__ == "__main__":
     parser.add_argument("--save_demo", type=ast.literal_eval, default=False)
     parser.add_argument("--save_video", type=ast.literal_eval, default=False)
     flags = parser.parse_args()
+    print(flags)
 
     start_time = time.time()
 
