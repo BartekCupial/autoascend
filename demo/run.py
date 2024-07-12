047b371260b2d4070586ddca0bf81cc6fbd7736f
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-07-12 06:58:16 +0200

    rewrite demo run to use map_parallel

--- demo/run.py
+++ demo/run.py
@@ -3,15 +3,14 @@ import ast
 import sys
 import time
 import traceback
-from multiprocessing import Pool
 from pathlib import Path
 
 import gym
-import numpy as np
 import pandas as pd
 
 from heur.run import EnvWrapper
 from nle_utils.collections import concat_dicts
+from nle_utils.parallel_utils import Result, map_parallel
 from nle_utils.utils import str2bool
 from nle_utils.wrappers import (
     FinalStatsWrapper,
@@ -23,48 +22,43 @@ from nle_utils.wrappers import (
 )
 
 
-def worker(args):
-    from_, to_, flags = args
+def worker(seed, game: str, savedir: str, gamesavedir: str, save_video: bool, save_demo: bool):
     orig_env = gym.make(
-        flags.game,
+        game,
         save_ttyrec_every=1,
-        savedir=flags.savedir,
+        savedir=savedir,
     )
     orig_env = TaskRewardsInfoWrapper(orig_env, done_only=False)
     orig_env = FinalStatsWrapper(orig_env, done_only=False)
     orig_env = TtyrecInfoWrapper(orig_env, done_only=False)
     orig_env = LastInfo(orig_env)
-    if flags.save_video:
-        orig_env = RenderTiles(orig_env, output_dir=flags.gamesavedir)
-    if flags.save_demo:
-        orig_env = NLEDemo(orig_env, flags.gamesavedir)
+    if save_video:
+        orig_env = RenderTiles(orig_env, output_dir=gamesavedir)
+    if save_demo:
+        orig_env = NLEDemo(orig_env, gamesavedir)
+    if seed is not None:
+        orig_env.seed(seed)
 
-    data = []
-    for i in range(from_, to_):
-        env = EnvWrapper(orig_env)
-        try:
-            if flags.seed:
-                orig_env.seed(i)
-            env.main()
-        except BaseException as e:
-            print(
-                "".join(traceback.format_exception(None, e, e.__traceback__)),
-                file=sys.stderr,
-            )
+    env = EnvWrapper(orig_env)
 
-        print(f"Run {i} finished with score {env.score}", file=sys.stderr)
+    try:
+        env.main()
+        error_message = None
+    except BaseException as e:
+        error_message = "".join(traceback.format_exception(None, e, e.__traceback__))
 
-        if flags.save_demo:
-            orig_env.save_to_file()
+    if save_demo:
+        orig_env.save_to_file()
 
-        data.append(orig_env.last_info.get("episode_extra_stats", {}))
+    message = f"{orig_env.get_seeds()}"
+    result = orig_env.last_info.get("episode_extra_stats", {})
+
     orig_env.close()
-    return data
+    return Result(result, description=message, log_msg=error_message)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--num_threads", type=int)
     parser.add_argument("--num_assessments", type=int)
     parser.add_argument("--savedir", type=str)
     parser.add_argument("--game", type=str)
@@ -73,26 +67,22 @@ if __name__ == "__main__":
     parser.add_argument("--seed", type=str2bool, default=True)
     parser.add_argument("--save_demo", type=ast.literal_eval, default=False)
     parser.add_argument("--save_video", type=ast.literal_eval, default=False)
+    parser.add_argument("--n_jobs", type=int, default=8)
+
     flags = parser.parse_args()
     print(flags)
 
     start_time = time.time()
 
-    with Pool(flags.num_threads) as pool:
-        data = list(
-            pool.map(
-                worker,
-                [
-                    (
-                        i * flags.num_assessments // flags.num_threads,
-                        (i + 1) * flags.num_assessments // flags.num_threads,
-                        flags,
-                    )
-                    for i in range(flags.num_threads)
-                ],
-            )
-        )
-    data = [s for ss in data for s in ss]
+    seeds = list(range(flags.num_assessments)) if flags.seed else [None] * flags.num_assessments
+    total = len(seeds)
+    data = map_parallel(
+        function=worker,
+        iterable=seeds,
+        function_args=(flags.game, flags.savedir, flags.gamesavedir, flags.save_video, flags.save_demo),
+        n_jobs=flags.n_jobs,
+    )
+
     df = pd.DataFrame.from_dict(concat_dicts(data), orient="index").T
     csv_output_path = Path(flags.gamesavedir) / "stats.csv"
     csv_output_path.parent.mkdir(exist_ok=True, parents=True)
