8435431da0c803e94c3601f0f457a482c4e8f598
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-07-14 20:02:42 +0200

    update flags, add option to pass character

--- demo/run.py
+++ demo/run.py
@@ -12,28 +12,23 @@ from heur.run import EnvWrapper
 from nle_utils.collections import concat_dicts
 from nle_utils.parallel_utils import Result, map_parallel
 from nle_utils.utils import str2bool
-from nle_utils.wrappers import (
-    FinalStatsWrapper,
-    LastInfo,
-    NLEDemo,
-    RenderTiles,
-    TaskRewardsInfoWrapper,
-    TtyrecInfoWrapper,
-)
+from nle_utils.wrappers import FinalStatsWrapper, NLEDemo, RenderVideo, TaskRewardsInfoWrapper, TtyrecInfoWrapper
 
 
-def worker(seed, game: str, savedir: str, gamesavedir: str, save_video: bool, save_demo: bool):
+def worker(
+    seed, game: str, character: str, savedir: str, gamesavedir: str, save_video: bool, save_demo: bool, show: bool
+):
     orig_env = gym.make(
         game,
+        character=character,
         save_ttyrec_every=1,
         savedir=savedir,
     )
     orig_env = TaskRewardsInfoWrapper(orig_env, done_only=False)
     orig_env = FinalStatsWrapper(orig_env, done_only=False)
     orig_env = TtyrecInfoWrapper(orig_env, done_only=False)
-    orig_env = LastInfo(orig_env)
     if save_video:
-        orig_env = RenderTiles(orig_env, output_dir=gamesavedir)
+        orig_env = RenderVideo(orig_env, output_dir=gamesavedir, show=show)
     if save_demo:
         orig_env = NLEDemo(orig_env, gamesavedir)
     if seed is not None:
@@ -62,11 +57,13 @@ if __name__ == "__main__":
     parser.add_argument("--num_assessments", type=int)
     parser.add_argument("--savedir", type=str)
     parser.add_argument("--game", type=str)
+    parser.add_argument("--character", type=str, default="@")
     parser.add_argument("--gamesavedir", type=str)
     parser.add_argument("--experiment_id", type=int, default=0)
     parser.add_argument("--seed", type=str2bool, default=True)
-    parser.add_argument("--save_demo", type=ast.literal_eval, default=False)
-    parser.add_argument("--save_video", type=ast.literal_eval, default=False)
+    parser.add_argument("--save_demo", type=str2bool, default=False)
+    parser.add_argument("--save_video", type=str2bool, default=False)
+    parser.add_argument("--show", type=str2bool, default=False)
     parser.add_argument("--n_jobs", type=int, default=8)
 
     flags = parser.parse_args()
@@ -79,7 +76,15 @@ if __name__ == "__main__":
     data = map_parallel(
         function=worker,
         iterable=seeds,
-        function_args=(flags.game, flags.savedir, flags.gamesavedir, flags.save_video, flags.save_demo),
+        function_args=(
+            flags.game,
+            flags.character,
+            flags.savedir,
+            flags.gamesavedir,
+            flags.save_video,
+            flags.save_demo,
+            flags.show,
+        ),
         n_jobs=flags.n_jobs,
     )
 
